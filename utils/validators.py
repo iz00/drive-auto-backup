daea6aa30fcf01bd01f7f4179466c0caa5b27e27
iz00 <isadoracontsost@gmail.com>
2025-08-20 16:06:51 -0300

    Add validator and parser for drive folder
    Add function to validate and return a given string
    as a valid Google Drive folder ID.
    Supports extracting the ID from a full URL or
    accepting a raw ID directly.
    Falls back to default folder when given string
    is empty or refers to root.

--- utils/validators.py
+++ utils/validators.py
@@ -1,4 +1,8 @@
 from pathlib import Path
+from re import fullmatch, search as regex_search
+
+
+DEFAULT_DRIVE_FOLDER = "root"
 
 
 def validate_and_get_absolute_local_path(path: str) -> str:
@@ -6,3 +10,16 @@ def validate_and_get_absolute_local_path(path: str) -> str:
         return str(Path(path).expanduser().resolve())
 
     raise ValueError("Invalid local path.")
+
+
+def validate_and_get_drive_folder_id(drive_folder: str) -> str:
+    if not drive_folder or drive_folder.lower() in ("/", "root", "home"):
+        return DEFAULT_DRIVE_FOLDER
+
+    if match := regex_search(r"(?:folders/|id=)(?P<id>[\w\-]+)", drive_folder):
+        return match.group("id")
+
+    if fullmatch(r"[\w\-]+", drive_folder):
+        return drive_folder
+
+    raise ValueError("Invalid Google Drive folder ID.")
