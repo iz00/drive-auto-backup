9f36845c8ff9b43c482de26efdfa04dd64116e5e
iz00 <isadoracontsost@gmail.com>
2025-09-03 21:36:05 -0300

    Allow user to inform empty default config values
    Since default configs can be empty, get the empty values
    and don't validate them, as validate_configs won't allow
    empty values.
    That way, user can set empty default configs and
    informed config values will still be validated.

--- commands/default.py
+++ commands/default.py
@@ -13,9 +13,18 @@ def default_handler(args: dict) -> None:
         print(error)
         return
 
-    if not (default_configs := validate_configs(args)):
+    # Empty default configs should be allowed, but validation doesn't allow them
+    empty_configs = {
+        config: "" for config, value in args.items() if value.strip() == ""
+    }
+    for key in empty_configs:
+        args.pop(key, None)
+
+    if not (default_configs := validate_configs(args)) and not empty_configs:
         print("All given defaults are invalid. Nothing was updated.")
         return
+
+    default_configs.update(empty_configs)
 
     # Update keys to match defaults in config file (begin with default_)
     for config in list(default_configs):
@@ -26,4 +35,5 @@ def default_handler(args: dict) -> None:
     save_config_to_file(total_configs)
 
     for config, value in default_configs.items():
+        value = value if value else '""'
         print(f"{config.replace('_', ' ').capitalize()} updated to {value}.")
